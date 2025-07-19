12f248e468ba61fed1ebf6552b1270275e3cbc13
maxim <bozzhik@gmail.com>
2025-07-19 16:18:53 +0300

    set up `tailwind` configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ export {metadata} from '@/lib/layout-config'
 import {plusJakartaSans} from '@/lib/layout-config'
 import '@/app/globals.css'
 
+import {cn} from '@/lib/utils'
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -9,7 +11,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${plusJakartaSans.variable} antialiased`}>{children}</body>
+      <body className={cn(plusJakartaSans.variable, 'bg-background text-foreground font-sans', 'antialiased')}>{children}</body>
     </html>
   )
 }
---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type {Config} from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+export default {
+  theme: {
+    screens: {
+      xl: {max: '1780px'},
+      sm: {max: '500px'},
+    },
+    fontFamily: {
+      sans: ['var(--font-plus-jakarta-sans)', ...defaultTheme.fontFamily.sans],
+    },
+    colors: ({colors}) => ({
+      background: '#100739',
+      foreground: '#D3D3F4',
+
+      transparent: colors.transparent,
+    }),
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
