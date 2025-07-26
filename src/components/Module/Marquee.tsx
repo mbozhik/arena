1d0c1595eeefa4cbee533e12520cb90a09345db1
maxim <bozzhik@gmail.com>
2025-07-26 20:44:36 +0300

    init `Marquee` Module component from `ground`
    component: https://ground.bossadizenith.me/docs/without-framer-motion/marquee
    source: https://github.com/mbozhik/orientir/blob/main/src/components/UI/Marquee.tsx

---
+++ src/components/Module/Marquee.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import {useMediaQuery} from '@/utils/use-media-query'
+import {cn} from '@/lib/utils'
+
+type MarqueeProps = {
+  className?: string
+  reverse?: boolean
+  pauseOnHover?: boolean
+  children?: React.ReactNode
+  vertical?: boolean
+  repeat?: number
+  [key: string]: React.ReactNode
+}
+
+export default function Marquee({className, reverse, pauseOnHover = false, children, vertical = false, repeat = 4, ...props}: MarqueeProps) {
+  const isDesktop = useMediaQuery('(min-width: 768px)')
+
+  return (
+    <div
+      {...props}
+      className={cn(
+        'group flex overflow-hidden px-2 [--duration:35s] [gap:var(--gap)]',
+        isDesktop ? '[--gap:5rem]' : '[--gap:2rem]',
+        {
+          'flex-row': !vertical,
+          'flex-col': vertical,
+        },
+        className,
+      )}
+    >
+      {Array(repeat)
+        .fill(0)
+        .map((_, i) => (
+          <div
+            key={i}
+            className={cn('flex shrink-0 justify-around [gap:var(--gap)]', {
+              'animate-marquee flex-row': !vertical,
+              'animate-marquee-vertical flex-col': vertical,
+              'group-hover:[animation-play-state:paused]': pauseOnHover,
+              '[animation-direction:reverse]': reverse,
+            })}
+          >
+            {children}
+          </div>
+        ))}
+    </div>
+  )
+}
