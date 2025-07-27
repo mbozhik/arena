d60fc5e5da0304fb83909852691a1078f22272df
maxim <bozzhik@gmail.com>
2025-07-27 14:17:39 +0300

    init `requests` file for sanity entites

---
+++ src/sanity/lib/requests.ts
@@ -0,0 +1,59 @@
+import {sanityFetch} from '@/sanity/lib/live'
+import {defineQuery} from 'next-sanity'
+import {draftMode} from 'next/headers'
+
+async function fetchEntity<T>(query: string, draft: boolean = true): Promise<T[]> {
+  try {
+    const {isEnabled} = await draftMode()
+
+    const response = await sanityFetch({
+      query,
+      ...(isEnabled && draft
+        ? {
+            perspective: 'drafts',
+            useCdn: false,
+            stega: true,
+          }
+        : undefined),
+    })
+    return (response.data as T[]) || []
+  } catch (error) {
+    console.log('Error fetching data:', error)
+    return []
+  }
+}
+
+async function fetchEntityItem<T>(query: string, params?: {slug?: string}, draft: boolean = false): Promise<T | null> {
+  try {
+    const {isEnabled} = await draftMode()
+    const response = await sanityFetch({
+      query,
+      params,
+      ...(isEnabled && draft
+        ? {
+            perspective: 'previewDrafts',
+            useCdn: false,
+            stega: true,
+          }
+        : undefined),
+    })
+    return (response.data as T) || null
+  } catch (error) {
+    console.log('Error fetching data:', error)
+    return null
+  }
+}
+
+const COURSE_QUERY = defineQuery(`
+    *[_type == "course"]{
+        category, heading, description, duration, rating, featured, slug, image,
+    }`)
+const COURSE_ITEM_QUERY = defineQuery(`
+    *[_type == "course" && slug.current == $slug][0]{
+        category, heading, description, duration, rating, featured, slug, image,
+    }`)
+
+const QUERIES = {
+  COURSE_QUERY,
+  COURSE_ITEM_QUERY,
+} as const
