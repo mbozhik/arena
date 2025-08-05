08a5e90555a8f2c8f84042a80931f28da42b21b9
maxim <bozzhik@gmail.com>
2025-08-05 14:24:57 +0700

    init `Grid` UI component

---
+++ src/components/UI/Grid.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+
+import {cn} from '@/lib/utils'
+
+const GRID = {
+  root: 'grid-cols-11',
+  large: 'col-span-7',
+  small: 'col-span-4',
+}
+
+function Grid({className, children, ...props}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <section data-section="grid" className={cn('grid gap-20', GRID.root, className)} {...props}>
+      {children}
+    </section>
+  )
+}
+
+function createArea(size: 'large' | 'small') {
+  const Component = ({className, children, ...props}: React.HTMLAttributes<HTMLDivElement>) => (
+    <div data-area={size} className={cn(GRID[size], className)} {...props}>
+      {children}
+    </div>
+  )
+  Component.displayName = `GridArea(${size.toUpperCase()})`
+  return Component
+}
+
+const LargeArea = createArea('large')
+const SmallArea = createArea('small')
+
+export {Grid, LargeArea, SmallArea}
